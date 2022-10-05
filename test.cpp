1f442277d5059f8a672ee399f825f61cffce150a
Sin0n0me <78182712+Sin0n0me@users.noreply.github.com>
2022-10-06 02:37:31 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,20 @@
+﻿// int型かshort型か決定する構造体 enable_ifを参考作成
+template <bool IsIntegerType>
+struct InputValue {};
+
+template <>
+struct InputValue<true> {
+	using Type = unsigned int;
+};
+
+template <>
+struct InputValue<false> {
+	using Type = unsigned short;
+};
+
+// 値が4バイトか2バイトか判定用の関数
+constexpr bool is4ByteType(const unsigned int i) { return i > 0xffff; }
+
+// 型の判定
+template <int value>
+using InputValue_t = typename InputValue<is4ByteType(value)>::Type;
